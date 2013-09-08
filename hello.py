ed0eb98d88aa423d0128d3db105250e02f416ee3
aqeel ahamad <aqeelahamad@Roselina.local>
2013-09-09 00:35:22 +0100

    hello

--- hello.py
+++ hello.py
@@ -2,8 +2,11 @@ import os
 from flask import Flask
 from flask import render_template
 
-TEMPLATE_DIRS = (os.path.join(PROJECT_DIR, "templates")
-
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, "templates"),
+    # here you can add another templates directory if you wish.
+)
 app = Flask(__name__)
 
 @app.route('/')
